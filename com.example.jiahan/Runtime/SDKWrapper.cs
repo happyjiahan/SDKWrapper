64e4ce46e4d2ee735b01c3ca7dd0fba3a902f4c2
JIAHAN LI <lijiahan@bilibili.com>
2020-11-03 17:23:18 +0800

    test

---
+++ com.example.jiahan/Runtime/SDKWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jiahan.SDK
+{
+public class SDKWrapper : MonoBehaviour
+{
+	public static void Test() {
+	}
+
+}
+}
+
